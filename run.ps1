3bc415b3bfd600ccc97f3420a768f26a415a27a9
bel-mont <cidhighwind85@hotmail.com>
2024-10-28 20:08:22 +0900

    postgres and docker files, empty db init

---
+++ run.ps1
@@ -0,0 +1,21 @@
+# Navigate to the directory containing the Dockerfile
+$dockerfilePath = "./docker/Dockerfile.postgres"
+$contextPath = "./docker"
+$imageName = "go-rest-test-postgres"
+$containerName = "go-rest-test-postgres-container"
+
+# Build the Docker image
+docker build -t $imageName -f $dockerfilePath $contextPath --no-cache
+
+# Check if a container with the same name is already running and stop it
+if (docker ps -a --format "{{.Names}}" | Select-String -Pattern $containerName) {
+    Write-Host "Stopping and removing existing container..."
+    docker stop $containerName
+    docker rm $containerName
+}
+
+# Run the Docker container
+docker run -d -p 5432:5432 --name $containerName $imageName
+
+# Output the status
+Write-Host "Docker container '$containerName' is now running."
