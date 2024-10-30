988acc8ad2f5b8fb2e383ae0564880bd65ffb978
bel-mont <cidhighwind85@hotmail.com>
2024-10-30 21:02:54 +0900

    sample text

--- handlers/home.go
+++ handlers/home.go
@@ -4,10 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func Home(c *gin.Context) {
 	c.HTML(200, "layout.html", gin.H{
-		"title":       "Home Page",
-		"description": "This is the home page of the website.",
-		"keywords":    "home, welcome, example",
-		"header":      "Welcome to My Website",
+		"title":       "SF6 Rankings",
+		"description": "Welcome to SF6 Rankings, where you can follow matches, leaderboards, and participate in the community.",
+		"keywords":    "sf6, rankings, matches, leaderboards",
+		"header":      "SF6 Rankings",
 		"content":     "home.html",
 	})
 }
